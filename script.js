fac726f4c2df2b7bec5dc48d075cbf94235b1479
MadarasiD <madarka0913@gmail.com>
2022-11-15 17:00:17 +0100

    update

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolEl = document.getElementById('symbol')
+const generateEl = document.getElementById('generate')
+const clipboardl = document.getElementById('clipboard')
+
+
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
 }
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value
+    const hasLower = lowercaseEl.checked
+    const hasUpper = uppercaseEl.checked
+    const hasNumber = numbersEl.checked
+    const hasSymbol = symbolEl.checked
+
+    resultEl.innerText = generatePAssword(hasLower, hasUpper, hasNumber, hasSymbol, length)
+})
+
+function generatePAssword(lower, upper, number, symbol, length) {
+    let generatePAssword = ''
+    const typesCount = lower + upper + number +symbol
+    console.log(typesCount)
+} 
 
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
